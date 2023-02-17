640feca4c53b671c41a6fc4edbe6450b9ce3afdb
Heba <hebaahmed97531@gmail.com>
2023-02-16 17:49:47 -0800

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Starting point
+ * Return: 1 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",59);
+	return (1);
+}
