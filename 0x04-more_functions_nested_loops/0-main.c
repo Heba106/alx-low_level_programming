888cf9467eccd4e4becc4152ae4001ec9bee417d
heba106 <hebaahmed97531@gmail.com>
2023-02-25 01:45:32 +0200

    is_upper

---
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	char c;
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
+}
