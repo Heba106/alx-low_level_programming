a19621246ea1362954fa16a64e6aa7fa89fbdf31
heba106 <hebaahmed97531@gmail.com>
2023-03-02 10:11:05 +0200

    Check betty style code

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,7 +4,7 @@
   * _strcat - concatenates two strings.
   * @dest: dest param
   * @src: src param
-  * 
+  *
   * Return: src pointer
   */
 
@@ -12,12 +12,12 @@ char *_strcat(char *dest, char *src)
 {
 	int len = 0;
 	int counter = 0;
-	
-	while ( *(dest + len) != '\0')
+
+	while (*(dest + len) != '\0')
 	{
 		len++;
 	}
-	while ( *(src + counter) != '\0')
+	while (*(src + counter) != '\0')
 	{
 		*(dest + len + counter) = *(src + counter);
 		counter++;
