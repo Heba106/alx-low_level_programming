9bd1848618d85887654af1eeaabe809532b925ab
heba106 <hebaahmed97531@gmail.com>
2023-03-02 10:07:17 +0200

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * _strcat - concatenates two strings.
+  * @dest: dest param
+  * @src: src param
+  * 
+  * Return: src pointer
+  */
+
+char *_strcat(char *dest, char *src)
+{
+	int len = 0;
+	int counter = 0;
+	
+	while ( *(dest + len) != '\0')
+	{
+		len++;
+	}
+	while ( *(src + counter) != '\0')
+	{
+		*(dest + len + counter) = *(src + counter);
+		counter++;
+	}
+
+	return (dest);
+}
+
