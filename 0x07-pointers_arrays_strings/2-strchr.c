6189f4b274fd8f52f838ef4bc7de40b905ba61f9
heba106 <hebaahmed97531@gmail.com>
2023-03-06 08:21:49 +0200

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * _strchr - func
+  * @s: param
+  * @c: param
+  * Return: pointer
+  */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (s + i);
+
+		i++;
+	}
+
+	return (0);
+}
