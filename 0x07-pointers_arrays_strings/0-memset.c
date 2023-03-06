e87e983de06f5b4f72958e2b4ff33979eaa97937
heba106 <hebaahmed97531@gmail.com>
2023-03-06 07:58:12 +0200

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * _memset - function
+  *
+  * @s: param
+  * @b: param
+  * @n: param
+  *
+  * Return: pointer
+  */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+
+	return (s);
+}
+
