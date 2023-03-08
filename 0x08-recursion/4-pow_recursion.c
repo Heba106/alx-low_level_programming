e4b99bf7121ad497f74b7e160e7900b4af5ba9dc
heba106 <hebaahmed97531@gmail.com>
2023-03-08 22:34:46 +0200

    pow_recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * pow_recursion - func
+  * @x: param
+  * @y: param
+  * Return: int
+  */
+int _pow_recursion(int x, int y)
+{
+	if (x == 1 || x == 0)
+		return (x);
+	if (y > 0)
+		return (x * _pow_recursion(x, y - 1));
+	else if ( y == 0)
+		return (1);
+	else
+		return(-1);
+}
