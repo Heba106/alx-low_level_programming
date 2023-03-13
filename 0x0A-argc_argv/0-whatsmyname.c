8207d7263769193a153a4d943f7ef1eb4c3d011c
heba106 <hebaahmed97531@gmail.com>
2023-03-13 19:54:43 +0200

    Write a program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
