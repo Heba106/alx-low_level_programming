fc053f88a3019294d953093eca0ad700f3917cf2
heba106 <hebaahmed97531@gmail.com>
2023-03-14 21:00:15 +0200

    The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * _strdup - function
+  * @str: param
+  * Return: return
+  */
+char *_strdup(char *str)
+{
+	int i = 0;
+	int len = 0;
+	char *res;
+	if (str == NULL)
+		return (NULL);
+	while(str[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	res = malloc(sizeof(char)*len);
+	if (res == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		res[i] = str[i];
+	return (res);
+}
+
