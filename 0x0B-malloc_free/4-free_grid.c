2cf4099abf8efbacc1cbca017fdad94fbdd69a45
heba106 <hebaahmed97531@gmail.com>
2023-03-15 20:28:03 +0200

    It's not bragging if you can back it up

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  * free_grid - func
+  * @height: param
+  * Return: return
+  */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
