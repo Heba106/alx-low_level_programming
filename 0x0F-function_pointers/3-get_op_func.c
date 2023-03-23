cfdb604a9db377475fdb1775aa26e301f98c07ae
heba106 <hebaahmed97531@gmail.com>
2023-03-23 14:25:56 +0200

    Write a program that performs simple operations.

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+  * get_op_func - func
+  * @s: param
+  * Return: func
+  */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].op)
+	{
+		if (s[0] == ops[i].op[0] && s[1] == '\0')
+		{
+			return (ops[i].f);
+			++i;
+		}
+
+	}
+	return (NULL);
+}
