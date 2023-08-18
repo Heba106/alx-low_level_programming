48514f417d620c110e23137443f07a2c024e1882
heba106 <hebaahmed97531@gmail.com>
2023-08-18 06:55:49 -0400

    who cares

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,16 +1,19 @@
 #include "dog.h"
+
 /**
-  * init_dog -  initialize a variable of type struct dog
-  * @d: puntero a la estructrua dog
-  * @name: puntero a la varaible name de la estructura dog
-  * @age: edad
-  * @owner: puntero a la variable owner de la estructrua dog
-  */
+ * init_dog - initializes a structure of type dog
+ * @d: pointer to structure
+ * @name: pointer to the name of the dog
+ * @age: age of dog
+ * @owner: pointer to owner of dog
+ *
+ * Return: void
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
-		(*d).name = name;
+		d->name = name;
 		d->age = age;
 		d->owner = owner;
 	}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,21 +1,30 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef DOG
+#define DOG
+
 /**
-  * struct dog - dog
-  * @name: its name
-  * @age: its age
-  * @owner: its owner
-  * Description: who cares?
-  */
+ * struct dog - dog class
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-int _putchar(char);
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/*dog_t *new_dog(char *name, float age, char *owner);*/
-/*void free_dog(dog_t *d);*/
-#endif /*ifndef DOG_H*/
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif
